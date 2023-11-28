07bf89f52a050847caba62c679a238abcb9fc7ba
Arkaannnn <hilmi.nizaar7@gmail.com>
2023-11-28 15:32:39 +0700

    Tugas pertemuan tanggal 27 Nop

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <center> Hak Cipta Oleh Arkaan </center>
+</footer>
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,28 +1,55 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
-<head>
+<head> --}}
+@extends('master')
+
+@section('title', 'Tambah Data Pegawai')
+
+@section('konten')
+
 	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
 </head>
 <body>
 
 	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Pegawai</h3>
+	<h3>Tambah Data Pegawai</h3>
 
-	<a href="/pegawai"> Kembali</a>
+	<a href="/pegawai" class=" btn btn-primary"> Kembali</a>
 
 	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+        <div class="form-group row">
+            <label for="name" class="col-xl-1 col-form-label mr-2">Nama</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+            </div>
+        <div class="form-group row">
+            <label for="jabatan" class="col-xl-1 col-form-label mr-2">Jabatan</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="jabatan" name="jabatan">
+            </div>
+         </div>
+        <div class="form-group row">
+            <label for="umur" class="col-xl-1 col-form-label mr-2">Umur</label>
+            <div class="col-xs-9">
+            <input type="number" class="form-control" id="umur" name="umur">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="alamat" class="col-xl-1 col-form-label mr-2">Alamat</label>
+            <div class="col-xs-9">
+            <textarea class="form-control" id="alamat" name="alamat"></textarea>
+            </div>
+        </div>
+		<input type="submit" value="Simpan Data" class = "btn btn-primary">
 	</form>
+@endsection
 
 
 
-</body>
-</html>
+{{-- </body>
+</html> --}}
